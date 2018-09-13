664635c136ecc48925043fd7a32fe01aaf4f0e0d
Markovic Vladimir <markovic.vladimir86@gmail.com>
2018-09-13 17:56:06 +0200

    Formatting code.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure	"1.9.0"]
-         								[org.vladimir/mongo-lib "0.1.0"]
-         								[org.vladimir/utils-lib "0.1.0"]
-         								]
+                 [org.vladimir/mongo-lib "0.1.0"]
+                 [org.vladimir/utils-lib "0.1.0"]
+                 ]
   :source-paths ["src/clj"])
 
