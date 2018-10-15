46270a64e852bb66b439c7ee78ebab4486f259d6
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2018-10-15 17:44:36 +0200

    Prepared project for clojars.

--- project.clj
+++ project.clj
@@ -1,11 +1,14 @@
-(defproject org.vladimir/dao-lib "0.1.0"
+(defproject org.clojars.vladimirmarkovic86/dao-lib "0.1.0"
   :description "Data access object library"
-  :url "http://gitlab:1610/VladimirMarkovic86/dao-lib"
+  :url "http://github.com/VladimirMarkovic86/dao-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.vladimir/db-lib "0.1.0"]
-                 [org.vladimir/utils-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/db-lib "0.1.0"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.1.0"]
                  ]
+
+  :min-lein-version "2.0.0"
+  
   :source-paths ["src/clj"])
 
