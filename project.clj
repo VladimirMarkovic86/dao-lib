d65ab2f7e00b1a9b63fe819f3d2a6962a6a1d703
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-02-06 14:19:38 +0100

    Task 38: Implemented calories calculator

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.4"
+(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.5"
   :description "Data access object library"
   :url "http://github.com/VladimirMarkovic86/dao-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.4"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.2"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.3"]
                  ]
 
   :min-lein-version "2.0.0"
