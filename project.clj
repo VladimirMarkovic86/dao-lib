951818860735bbc66769b3adebe4e2679725674b
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 13:53:42 +0200

    Task 88: Removed README.md and LICENSE files from packed jar files.

--- project.clj
+++ project.clj
@@ -1,17 +1,20 @@
-(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.26"
+(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.27"
   :description "Data access object library"
   :url "http://github.com/VladimirMarkovic86/dao-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.12"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.26"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.10"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.11"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"])
+  :test-paths ["test/clj"]
+  
+  :jar-exclusions [#"README.md$"
+                   #"LICENSE$"])
 
