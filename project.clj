74fda6ad4d2f129fc68e084c7c43dfa225b0b8ce
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-07-15 18:01:07 +0200

    Task 88: Rolled back README.md and LICENSE files to appear in jar files.

--- project.clj
+++ project.clj
@@ -1,20 +1,17 @@
-(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.27"
+(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.28"
   :description "Data access object library"
   :url "http://github.com/VladimirMarkovic86/dao-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.13"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.27"]
-                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.11"]
-                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.12"]
+                 [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.28"]
+                 [org.clojars.vladimirmarkovic86/mongo-lib "0.2.12"]
+                 [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
                  ]
 
   :min-lein-version "2.0.0"
   
   :source-paths ["src/clj"]
-  :test-paths ["test/clj"]
-  
-  :jar-exclusions [#"README.md$"
-                   #"LICENSE$"])
+  :test-paths ["test/clj"])
 
