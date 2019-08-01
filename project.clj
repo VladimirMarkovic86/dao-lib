7444dbfb753070736ea824e7155d73890492be3a
VladimirMarkovic86 <markovic.vladimir86@gmail.com>
2019-08-01 13:18:04 +0200

    Task 95: Refactored the way routing works.

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.29"
+(defproject org.clojars.vladimirmarkovic86/dao-lib "0.3.30"
   :description "Data access object library"
   :url "http://github.com/VladimirMarkovic86/dao-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojars.vladimirmarkovic86/ajax-lib "0.1.14"]
-                 [org.clojars.vladimirmarkovic86/session-lib "0.2.29"]
+                 [org.clojars.vladimirmarkovic86/session-lib "0.2.30"]
                  [org.clojars.vladimirmarkovic86/mongo-lib "0.2.13"]
                  [org.clojars.vladimirmarkovic86/utils-lib "0.4.13"]
                  ]
